ac429dffb32a8827c7cc0a3c0fa3ae24e669bb3d
Oak Soe Htet <oaksoe.h@ku.th>
2024-03-12 15:41:08 +0700

    added docstrings

--- circle.py
+++ circle.py
@@ -9,6 +9,15 @@ class Circle:
         
         :param radius: radius of the circle, may be zero.
         :raises ValueError: if radius is negative.
+
+        >>> Circle(3).radius
+        3
+        >>> Circle(4).radius
+        4
+        >>> Circle(-1)
+        Traceback (most recent call last):
+        ...
+        ValueError: radius must be non-negative
         """
         if radius < 0:
             raise ValueError("radius must be non-negative")
@@ -19,6 +28,14 @@ class Circle:
         area of this circle and another circle.
         Since area is pi*r**2, the radii of the 3 circles
         should form a Pythagorean triple (r1^2 + r2^2 = r3^2)
+
+        >>> c1 = Circle(3)
+        >>> c2 = Circle(4)
+        >>> c3 = c1.add_area(c2)
+        >>> c3.radius
+        5.0
+        >>> c1.get_area() + c2.get_area() == c3.get_area()
+        True
         """
         r1 = self.get_radius()
         r2 = circle.get_radius()
